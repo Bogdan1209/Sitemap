a7ebeb7f5632cff93ca8130ef145ce30ed93549e
Bogdan1209 <bogdan1209@rambler.ru>
2016-10-22 14:31:49 +0300

    why method stop?

---
+++ Sitemap.Presentation/App_Start/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+using Sitemap.Presentation.Models;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity;
+
+[assembly: OwinStartup(typeof(Sitemap.Presentation.Startup))]
+
+namespace Sitemap.Presentation
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // SignalR
+            app.MapSignalR();
+            // Context'n'Manager for auth
+            app.CreatePerOwinContext<ApplicationContext>(ApplicationContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+        }
+    }
+}
